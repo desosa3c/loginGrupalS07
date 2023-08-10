d3edd2586e76061f2d8d244bd80eec5a478da652
desosa3c <141156723+desosa3c@users.noreply.github.com>
2023-08-10 09:54:25 -0300

    Correccion de errores validacion.js

--- js/validacion.js
+++ js/validacion.js
@@ -23,20 +23,3 @@ regBtn.addEventListener("click", () => {
         showAlertSuccess();
     }
 });
-1234567
-
-
-
-
-regBtn.addEventListener('click', () => {
-    let nombre = document.getElementById('nombre').value;
-    let apellido = document.getElementById('apellido').value;
-    let email = document.getElementById('email').value;
-    let pw1 = document.getElementById('password1').value;
-    let pw2 = document.getElementById('password2').value;
-    let terminos = document.getElementById('terminos').checked;
-    const regBtn = document.getElementById('regBtn')
-    if (nombre.trim() === '' || apellido.trim() === '' || pw1.trim() === '' || pw1 !== pw2 || pw1.length < 6 || terminos === false) {
-        showAlertError()
-    } else { showAlertSuccess() }
-})
